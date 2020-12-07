9ed92f8846005dd22b31de8b9392272f88572678
grusic91 <gasper.rus@programmfabrik.de>
2020-12-07 20:13:55 +0100

    creates simple quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,85 @@
 import 'package:flutter/material.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  var questionIndex = 0;
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
 
-  void answerQuestion() {
-    questionIndex = questionIndex + 1;
-    print(questionIndex);
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 7},
+        {'text': 'Green', 'score': 5},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': [
+        {'text': 'Rabit', 'score': 10},
+        {'text': 'Snake', 'score': 4},
+        {'text': 'Elephant', 'score': 7},
+        {'text': 'Lion', 'score': 2},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite instruction?',
+      'answers': [
+        {'text': 'Max', 'score': 10},
+        {'text': 'Max', 'score': 10},
+        {'text': 'Max', 'score': 10},
+        {'text': 'Max', 'score': 10},
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuit() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var queations = [
-      'What\'s your favourite color?',
-      'What\'s your favourite animal?',
-    ];
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('My firsst App Hello World!'),
-            ),
-            body: Column(
-              children: [
-                Container(
-                    child: Center(
-                  child: Text(
-                    'Hello World!',
-                    style:
-                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-                  ),
-                )),
-                Text(
-                  queations[questionIndex],
-                ),
-                RaisedButton(
-                  child: Text('HELLO WORLD!'),
-                  onPressed: answerQuestion,
-                )
-              ],
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My firsst App Hello World!'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuit),
+      ),
+    );
   }
 }
