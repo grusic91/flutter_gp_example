e52b5ea6f772d03b1b0361dd55eeec36ff93ed99
grusic91 <grusic91@users.noreply.github.com>
2020-12-07 10:52:24 +0000

    deploy: ecbf19f5a54709e82b08183eb154568823320af8

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0eb4ba659b7c0f51ecb52aab378db0bd",
-"/": "0eb4ba659b7c0f51ecb52aab378db0bd",
-"main.dart.js": "b123bd9a1c3c764e91de61aed2469633",
+"index.html": "9e73abc4c06973a77bd7a180f8aee9c6",
+"/": "9e73abc4c06973a77bd7a180f8aee9c6",
+"main.dart.js": "bdc796ff350dde285585688b15cf3dee",
 "manifest.json": "31ca8053c954bb06789fa7c224793534",
 "version.json": "7602971a31d58a9d4a6ce47aed860828"
 };
